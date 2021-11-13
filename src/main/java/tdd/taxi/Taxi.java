3103a2ace4b0a4e589e4fe2ec112cdaef27df272
jiabao Dong <jiabao.dong@thoughtworks.com>
2021-11-13 13:28:46 +0800

    抽取常量

--- src/main/java/tdd/taxi/Taxi.java
+++ src/main/java/tdd/taxi/Taxi.java
@@ -4,6 +4,7 @@ public class Taxi {
 
     private static final double UNIT_PRICE = 0.8;
     public static final int BASE_DISTANCE = 8;
+    public static final double UNIT_PRICE_WAIT = 0.25;
 
     public double calculate(int waitingTime, int distance) {
         double distancePrice = UNIT_PRICE * distance;
@@ -11,7 +12,7 @@ public class Taxi {
         if (distance > BASE_DISTANCE) {
             longDistanceFee = (distance - BASE_DISTANCE) * (UNIT_PRICE / 2);
         }
-        double waitingFee = waitingTime * 0.25;
+        double waitingFee = waitingTime * UNIT_PRICE_WAIT;
         return distancePrice + longDistanceFee + waitingFee;
     }
 }
