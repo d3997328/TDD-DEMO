6912ed6c2e59f634e3c722ed185f36291cc541ae
jiabao Dong <jiabao.dong@thoughtworks.com>
2021-11-13 13:15:32 +0800

    超过8公里则每公里加收50%长途费

--- src/main/java/tdd/taxi/Taxi.java
+++ src/main/java/tdd/taxi/Taxi.java
@@ -3,8 +3,14 @@ package tdd.taxi;
 public class Taxi {
 
     private static final double UNIT_PRICE = 0.8;
+    public static final int BASE_DISTANCE = 8;
 
     public double calculate(int distance) {
-        return UNIT_PRICE * distance;
+        double distancePrice = UNIT_PRICE * distance;
+        double longDistanceFee = 0;
+        if (distance > BASE_DISTANCE) {
+            longDistanceFee = (distance - BASE_DISTANCE) * (UNIT_PRICE / 2);
+        }
+        return distancePrice + longDistanceFee;
     }
 }
